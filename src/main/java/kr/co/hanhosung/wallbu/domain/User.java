873e6af67f4821b66f00b1e58fc1984f853edb8f
hanhosung <winsomed96@naver.com>
2024-06-13 16:00:20 +0900

    fix: update user domain unique constraint

--- src/main/java/kr/co/hanhosung/wallbu/domain/User.java
+++ src/main/java/kr/co/hanhosung/wallbu/domain/User.java
@@ -7,6 +7,7 @@ import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 
+import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.EnumType;
 import javax.persistence.Enumerated;
@@ -29,6 +30,7 @@ public class User  extends BaseEntity{
 
     private String email;
 
+    @Column(unique = true)
     private String phoneNumber;
 
     private String password;
