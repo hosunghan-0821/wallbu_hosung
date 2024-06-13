69e7f1b9ee2af4bc75df16104cab111d20b82324
hanhosung <winsomed96@naver.com>
2024-06-13 21:08:59 +0900

    fix: update cofing files

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -15,4 +15,20 @@ VALUES ('winsomed31@naver.com', '이구성', '952f15428895417265636f84ae9e7dfeb2
 INSERT INTO tb_user(email,name,password,phone_number,user_role)
 VALUES ('winsomed33@naver.com', '이새미', '952f15428895417265636f84ae9e7dfeb2e937ee0fc24cdd8c015b4e54a701e9', '01095756304','PROFESSOR');
 
---User 정보 자동 insert
+--강좌 정보 자동 insert
+INSERT INTO tb_lecture(title, student_count, max_student_count, price)
+VALUES ('강좌1', 2, 10, 10000);
+
+INSERT INTO tb_lecture(title, student_count, max_student_count, price)
+VALUES ('강좌2', 1, 10, 10000);
+
+INSERT INTO tb_lecture(title, student_count, max_student_count, price)
+VALUES ('강좌3', 3, 20, 10000);
+
+INSERT INTO tb_lecture(title, student_count, max_student_count, price)
+VALUES ('강좌4', 9, 10, 10000);
+
+INSERT INTO tb_lecture(title, student_count, max_student_count, price)
+VALUES ('강좌5', 0, 10, 10000);
+
+
