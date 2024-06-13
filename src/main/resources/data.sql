1e9892c1240d69b859bf59a939138d40a912ae88
hanhosung <winsomed96@naver.com>
2024-06-13 15:58:03 +0900

    fe: add data init sql

---
+++ src/main/resources/data.sql
@@ -0,0 +1,18 @@
+
+--User 정보 자동 insert // 비밀번호 ghtjd114 암호환 것
+INSERT INTO tb_user(email,name,password,phone_number,user_role)
+VALUES ('winsomed6@naver.com', '한호성', '952f15428895417265636f84ae9e7dfeb2e937ee0fc24cdd8c015b4e54a701e9', '01095756300','STUDENT');
+
+INSERT INTO tb_user(email,name,password,phone_number,user_role)
+VALUES ('winsomed96@naver.com', '김포성', '952f15428895417265636f84ae9e7dfeb2e937ee0fc24cdd8c015b4e54a701e9', '01095756301','STUDENT');
+
+INSERT INTO tb_user(email,name,password,phone_number,user_role)
+VALUES ('winsomed32@naver.com', '이포성', '952f15428895417265636f84ae9e7dfeb2e937ee0fc24cdd8c015b4e54a701e9', '01095756302','PROFESSOR');
+
+INSERT INTO tb_user(email,name,password,phone_number,user_role)
+VALUES ('winsomed31@naver.com', '이구성', '952f15428895417265636f84ae9e7dfeb2e937ee0fc24cdd8c015b4e54a701e9', '01095756303','PROFESSOR');
+
+INSERT INTO tb_user(email,name,password,phone_number,user_role)
+VALUES ('winsomed33@naver.com', '이새미', '952f15428895417265636f84ae9e7dfeb2e937ee0fc24cdd8c015b4e54a701e9', '01095756304','PROFESSOR');
+
+--User 정보 자동 insert
