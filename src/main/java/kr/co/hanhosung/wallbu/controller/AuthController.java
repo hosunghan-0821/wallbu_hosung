6e6f521ef5c498e4dada1ef6d2011d725145b0a3
hanhosung <winsomed96@naver.com>
2024-06-15 17:42:52 +0900

    fe: add swagger more info annotation

--- src/main/java/kr/co/hanhosung/wallbu/controller/AuthController.java
+++ src/main/java/kr/co/hanhosung/wallbu/controller/AuthController.java
@@ -1,5 +1,8 @@
 package kr.co.hanhosung.wallbu.controller;
 
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import kr.co.hanhosung.wallbu.dto.LoginDto;
 import kr.co.hanhosung.wallbu.dto.TokenDto;
 import kr.co.hanhosung.wallbu.dto.UserDto;
@@ -19,6 +22,7 @@ import org.springframework.web.bind.annotation.RestController;
 import javax.servlet.http.HttpServletRequest;
 import javax.validation.Valid;
 
+@Tag(name = "유저 인증 컨트롤러", description = "유저 인증 API들이 존재합니다. (로그인, 회원가입, 토큰 재발행)")
 @RestController
 @Slf4j
 @RequiredArgsConstructor
@@ -29,6 +33,9 @@ public class AuthController {
 
     private final AuthService authService;
 
+    @Operation(summary = "회원가입API")
+    @ApiResponse(responseCode = "201",description = "회원가입 성공")
+    @ApiResponse(responseCode = "400",description = "회원가입 실패 유효성 검사 or 중복유저")
     @PostMapping("/auth/signup")
     public ResponseEntity<Boolean> signUp(@Valid @RequestBody UserDto userDto) {
         assert (userDto != null);
@@ -38,6 +45,9 @@ public class AuthController {
         return new ResponseEntity<>(Boolean.TRUE, HttpStatus.CREATED);
     }
 
+    @Operation(summary = "로그인 API")
+    @ApiResponse(responseCode = "200",description = "로그인 성공 - 성공시 헤더에 토큰값 저장해서 반환")
+    @ApiResponse(responseCode = "400",description = "로그인 정보 유효성 검사 실패 or 유저정보 DB 존재 X")
     @PostMapping("/auth/login")
     public ResponseEntity<Void> login(@Valid @RequestBody LoginDto loginDto) {
 
@@ -57,6 +67,9 @@ public class AuthController {
     }
 
 
+    @Operation(summary = "새로운 access token 발급 API")
+    @ApiResponse(responseCode = "200",description = "access token 발급 성공 - 성공시 헤더에 토큰값 저장해서 반환")
+    @ApiResponse(responseCode = "401",description = "토큰 정보 유효하지 않음 or 누락")
     @GetMapping("/auth/refresh")
     public ResponseEntity<Void> getAccessTokenByRefreshToken(HttpServletRequest request) {
 
--- src/main/java/kr/co/hanhosung/wallbu/controller/LectureController.java
+++ src/main/java/kr/co/hanhosung/wallbu/controller/LectureController.java
@@ -1,6 +1,10 @@
 package kr.co.hanhosung.wallbu.controller;
 
 
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.Parameter;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import kr.co.hanhosung.wallbu.dto.EnrollDto;
 import kr.co.hanhosung.wallbu.dto.LectureDto;
 import kr.co.hanhosung.wallbu.global.annotation.Login;
@@ -25,6 +29,7 @@ import java.util.List;
 
 import static kr.co.hanhosung.wallbu.global.validation.ValidationMarker.*;
 
+@Tag(name = "강의 컨트롤러", description = "강의 도메인 관련 API들이 존재합니다. ( 강좌등록(강사), 강좌신청(수강생,강사), 강좌조회)")
 @RestController
 @Slf4j
 @RequiredArgsConstructor
@@ -34,8 +39,12 @@ public class LectureController {
     private final LectureService lectureService;
 
 
+    @Operation(summary = "강좌 등록 API (강사)")
+    @ApiResponse(responseCode = "201", description = "강좌 등록 성공")
+    @ApiResponse(responseCode = "401", description = "유저 토큰 유효하지 않음")
+    @ApiResponse(responseCode = "400", description = "유저 ROLE 오류")
     @PostMapping("/lectures")
-    public ResponseEntity<Boolean> registerLecture(@Validated({OnRegisterLecture.class}) @RequestBody LectureDto lectureDto, @Login Long userId) {
+    public ResponseEntity<Boolean> registerLecture(@Validated({OnRegisterLecture.class}) @RequestBody LectureDto lectureDto, @Parameter(hidden = true) @Login Long userId) {
         assert (lectureDto != null);
         assert (userId != null);
 
@@ -43,8 +52,12 @@ public class LectureController {
         return new ResponseEntity<>(Boolean.TRUE, HttpStatus.CREATED);
     }
 
+    @Operation(summary = "강의 신청 API (강사,수강생)")
+    @ApiResponse(responseCode = "200", description = "강의 등록 성공")
+    @ApiResponse(responseCode = "401", description = "유저 토큰 유효하지 않음")
+    @ApiResponse(responseCode = "400", description = "강좌 만원 or 중복 수강신청 or 강좌 신청 유효성 검사 실패")
     @PostMapping("/lectures/enroll")
-    public ResponseEntity<Boolean> enrollmentLecture(@Validated({OnEnrollLecture.class}) @RequestBody EnrollDto enrollDto, @Login Long userId) {
+    public ResponseEntity<Boolean> enrollmentLecture(@Validated({OnEnrollLecture.class}) @RequestBody EnrollDto enrollDto, @Parameter(hidden = true) @Login Long userId) {
         assert (enrollDto != null);
         assert (userId != null);
 
@@ -53,8 +66,11 @@ public class LectureController {
         return new ResponseEntity<>(Boolean.TRUE, HttpStatus.OK);
     }
 
+    @Operation(summary = "강좌 조회 API ")
+    @ApiResponse(responseCode = "200", description = "강좌 조회")
+    @ApiResponse(responseCode = "401", description = "유저 토큰 유효하지 않음")
     @GetMapping("/lectures")
-    public ResponseEntity<Page<LectureDto>> getLectureList(Pageable pageable, @RequestParam(value = "sort", defaultValue = "RECENTLY_REGISTERED") SortingType sortingType) {
+    public ResponseEntity<Page<LectureDto>> getLectureList(@Parameter(hidden = true) Pageable pageable, @RequestParam(value = "sort", defaultValue = "RECENTLY_REGISTERED") SortingType sortingType) {
 
         Page<LectureDto> lectureList = lectureService.getLectureList(pageable, sortingType);
         return new ResponseEntity<>(lectureList, HttpStatus.OK);
